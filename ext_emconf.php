31cbd072b6080db06d296e77f419e3710c415d52
Konstantin Hillmann <k.hillmann@familie-redlich.de>
2024-10-17 16:31:19 +0200

    [TASK] adjust minimal requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array (
         array (
             'depends' =>
                 array (
-                    'typo3' => '12.4.0-12.4.99',
+                    'typo3' => '12.4.22-12.4.99',
                 ),
             'conflicts' =>
                 array (
