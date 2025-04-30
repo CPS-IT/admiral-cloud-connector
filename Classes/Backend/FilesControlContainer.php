4ffe28d68c9cf37087669f9187cb65f11faf8943
Elias Häußler <e.haeussler@familie-redlich.de>
2025-04-30 07:45:43 +0200

    [TASK] Upgrade FilesControlContainer to TYPO3 v13

--- Classes/Backend/FilesControlContainer.php
+++ Classes/Backend/FilesControlContainer.php
@@ -1,31 +1,33 @@
 <?php
 
-namespace CPSIT\AdmiralCloudConnector\Backend;
+declare(strict_types=1);
 
 /*
- * This source file is proprietary property of Beech.it
- * Date: 19-2-18
- * All code (c) Beech.it all rights reserved
+ * This file is part of the TYPO3 CMS extension "admiral_cloud_connector".
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
  */
 
-
+namespace CPSIT\AdmiralCloudConnector\Backend;
 
 use CPSIT\AdmiralCloudConnector\Resource\AdmiralCloudDriver;
-use CPSIT\AdmiralCloudConnector\Utility\ConfigurationUtility;
-use CPSIT\AdmiralCloudConnector\Utility\PermissionUtility;
 use Psr\EventDispatcher\EventDispatcherInterface;
-use TYPO3\CMS\Backend\Form\Container\FileReferenceContainer;
-use TYPO3\CMS\Backend\Form\Event\CustomFileControlsEvent;
 use TYPO3\CMS\Backend\Form\InlineStackProcessor;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
-use TYPO3\CMS\Backend\Utility\BackendUtility;
-use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Crypto\HashService;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Imaging\IconSize;
 use TYPO3\CMS\Core\Page\JavaScriptModuleInstruction;
+use TYPO3\CMS\Core\Resource\DefaultUploadFolderResolver;
 use TYPO3\CMS\Core\Resource\Filter\FileExtensionFilter;
-use TYPO3\CMS\Core\Resource\ResourceStorage;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Utility\MathUtility;
-use TYPO3Fluid\Fluid\View\TemplateView;
+use TYPO3\CMS\Core\Resource\OnlineMedia\Helpers\OnlineMediaHelperRegistry;
 
 /**
  * Class InlineControlContainer
@@ -34,17 +36,35 @@ use TYPO3Fluid\Fluid\View\TemplateView;
  */
 class FilesControlContainer extends \TYPO3\CMS\Backend\Form\Container\FilesControlContainer
 {
+    public function __construct(
+        protected readonly IconFactory $iconFactory,
+        protected readonly InlineStackProcessor $inlineStackProcessor,
+        protected readonly UriBuilder $uriBuilder,
+        EventDispatcherInterface $eventDispatcher,
+        OnlineMediaHelperRegistry $onlineMediaHelperRegistry,
+        DefaultUploadFolderResolver $defaultUploadFolderResolver,
+        HashService $hashService,
+    ) {
+        parent::__construct(
+            $iconFactory,
+            $inlineStackProcessor,
+            $eventDispatcher,
+            $onlineMediaHelperRegistry,
+            $defaultUploadFolderResolver,
+            $hashService,
+        );
+    }
+
     protected function getFileSelectors(array $inlineConfiguration, FileExtensionFilter $fileExtensionFilter): array
     {
         $controls = parent::getFileSelectors($inlineConfiguration, $fileExtensionFilter);
         $controls[] = $this->renderAdmiralCloudOverviewButton();
         $controls[] = $this->renderAdmiralCloudUploadButton();
+
         return $controls;
     }
 
-
     /**
-     * @return string
      * @throws \TYPO3\CMS\Backend\Routing\Exception\RouteNotFoundException
      */
     protected function renderAdmiralCloudOverviewButton(): string
@@ -54,7 +74,7 @@ class FilesControlContainer extends \TYPO3\CMS\Backend\Form\Container\FilesContr
         if (!$this->admiralCloudStorageAvailable()) {
             $errorTextHtml = [];
             $errorTextHtml[] = '<div class="alert alert-danger" style="display: inline-block">';
-            $errorTextHtml[] = $this->iconFactory->getIcon('actions-admiral_cloud-browser', Icon::SIZE_SMALL)->render();
+            $errorTextHtml[] = $this->iconFactory->getIcon('actions-admiral_cloud-browser', IconSize::SMALL)->render();
             $errorTextHtml[] = htmlspecialchars($languageService->sL('LLL:EXT:admiral_cloud_connector/Resources/Private/Language/locallang_be.xlf:browser.error-no-storage-access'));
             $errorTextHtml[] = '</div>';
 
@@ -64,10 +84,9 @@ class FilesControlContainer extends \TYPO3\CMS\Backend\Form\Container\FilesContr
         $foreign_table = 'sys_file_reference';
         $currentStructureDomObjectIdPrefix = $this->inlineStackProcessor->getCurrentStructureDomObjectIdPrefix($this->data['inlineFirstPid']);
 
-        $element = 'admiral_cloud' . md5($currentStructureDomObjectIdPrefix);
+        $element = 'admiral_cloud' . md5((string)$currentStructureDomObjectIdPrefix);
 
-        $uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
-        $compactViewUrl = $uriBuilder->buildUriFromRoute('admiral_cloud_browser_show', [
+        $compactViewUrl = (string)$this->uriBuilder->buildUriFromRoute('admiral_cloud_browser_show', [
             'element' => $element,
             'irreObject' => $currentStructureDomObjectIdPrefix . '-' . $foreign_table,
         ]);
@@ -80,14 +99,14 @@ class FilesControlContainer extends \TYPO3\CMS\Backend\Form\Container\FilesContr
         $buttonHtml[] = '<a href="#" class="btn btn-default t3js-admiral_cloud-browser-btn overview ' . $element . '"'
             . ' data-admiral_cloud-browser-url="' . htmlspecialchars($compactViewUrl) . '" '
             . ' data-title="' . htmlspecialchars($titleText) . '">';
-        $buttonHtml[] = $this->iconFactory->getIcon('actions-admiral_cloud-browser', Icon::SIZE_SMALL)->render();
+        $buttonHtml[] = $this->iconFactory->getIcon('actions-admiral_cloud-browser', IconSize::SMALL)->render();
         $buttonHtml[] = $buttonText;
         $buttonHtml[] = '</a>';
+
         return LF . implode(LF, $buttonHtml);
     }
 
     /**
-     * @return string
      * @throws \TYPO3\CMS\Backend\Routing\Exception\RouteNotFoundException
      */
     protected function renderAdmiralCloudUploadButton(): string
@@ -97,7 +116,7 @@ class FilesControlContainer extends \TYPO3\CMS\Backend\Form\Container\FilesContr
         if (!$this->admiralCloudStorageAvailable()) {
             $errorTextHtml = [];
             $errorTextHtml[] = '<div class="alert alert-danger" style="display: inline-block">';
-            $errorTextHtml[] = $this->iconFactory->getIcon('actions-admiral_cloud-browser', Icon::SIZE_SMALL)->render();
+            $errorTextHtml[] = $this->iconFactory->getIcon('actions-admiral_cloud-browser', IconSize::SMALL)->render();
             $errorTextHtml[] = htmlspecialchars($languageService->sL('LLL:EXT:admiral_cloud_connector/Resources/Private/Language/locallang_be.xlf:browser.error-no-storage-access'));
             $errorTextHtml[] = '</div>';
 
@@ -107,10 +126,9 @@ class FilesControlContainer extends \TYPO3\CMS\Backend\Form\Container\FilesContr
         $foreign_table = 'sys_file_reference';
         $currentStructureDomObjectIdPrefix = $this->inlineStackProcessor->getCurrentStructureDomObjectIdPrefix($this->data['inlineFirstPid']);
 
-        $element = 'admiral_cloud' . md5($currentStructureDomObjectIdPrefix);
+        $element = 'admiral_cloud' . md5((string)$currentStructureDomObjectIdPrefix);
 
-        $uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
-        $compactViewUrl = $uriBuilder->buildUriFromRoute('admiral_cloud_browser_upload', [
+        $compactViewUrl = (string)$this->uriBuilder->buildUriFromRoute('admiral_cloud_browser_upload', [
             'element' => $element,
             'irreObject' => $currentStructureDomObjectIdPrefix . '-' . $foreign_table,
         ]);
@@ -123,15 +141,15 @@ class FilesControlContainer extends \TYPO3\CMS\Backend\Form\Container\FilesContr
         $buttonHtml[] = '<a href="#" class="btn btn-default t3js-admiral_cloud-browser-btn upload ' . $element . '"'
             . ' data-admiral_cloud-browser-url="' . htmlspecialchars($compactViewUrl) . '" '
             . ' data-title="' . htmlspecialchars($titleText) . '">';
-        $buttonHtml[] = $this->iconFactory->getIcon('actions-admiral_cloud-browser', Icon::SIZE_SMALL)->render();
+        $buttonHtml[] = $this->iconFactory->getIcon('actions-admiral_cloud-browser', IconSize::SMALL)->render();
         $buttonHtml[] = $buttonText;
         $buttonHtml[] = '</a>';
+
         return LF . implode(LF, $buttonHtml);
     }
 
     protected function admiralCloudStorageAvailable(): bool
     {
-        /** @var ResourceStorage $fileStorage */
         foreach ($this->getBackendUserAuthentication()->getFileStorages() as $fileStorage) {
             if ($fileStorage->getDriverType() === AdmiralCloudDriver::KEY) {
                 return true;
@@ -139,6 +157,4 @@ class FilesControlContainer extends \TYPO3\CMS\Backend\Form\Container\FilesContr
         }
         return false;
     }
-
-
 }
