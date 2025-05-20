c066833ea43ecc57e783eb74c2183aced6c9dcd2
Elias Häußler <e.haeussler@familie-redlich.de>
2025-05-20 08:43:05 +0200

    [TASK] Refine PHP-CS-Fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,11 +2,29 @@
 
 declare(strict_types=1);
 
-$config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect());
+/*
+ * This file is part of the TYPO3 CMS extension "admiral_cloud_connector".
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+use TYPO3\CodingStandards\CsFixerConfig;
+
+$config = CsFixerConfig::create();
+$config->setParallelConfig(ParallelConfigFactory::detect());
 $config->setHeader('This file is part of the TYPO3 CMS extension "admiral_cloud_connector".');
 $config->getFinder()
     ->in(__DIR__)
+    ->ignoreDotFiles(false)
+    ->ignoreVCSIgnored(true)
 ;
 
 return $config;
